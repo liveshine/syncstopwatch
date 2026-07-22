73582f52df839a6a092fbd73ded964c7defba072
liveshine <liveshine12@gmail.com>
2026-07-22 10:31:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,31 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
+const fs = require('fs'); // Added filesystem module to check files
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Point exactly to the public folder
+const publicPath = path.join(__dirname, 'public');
+app.use(express.static(publicPath));
+
+// BULLETPROOF ROUTING: If the file is missing, tell you exactly why
+app.get('*', (req, res) => {
+  const indexPath = path.join(publicPath, 'index.html');
+  if (fs.existsSync(indexPath)) {
+    res.sendFile(indexPath);
+  } else {
+    // This will print on your screen if the folder structure is wrong
+    res.status(404).send(`
+      <h2 style="font-family: sans-serif;">Error: Missing File</h2>
+      <p style="font-family: sans-serif;">The server is awake, but it can't find your HTML file.</p>
+      <p style="font-family: sans-serif;">It is strictly looking inside this exact folder path:<br> <b>${publicPath}</b></p>
+      <p style="font-family: sans-serif;">Ensure your folder in GitHub is named exactly "<b>public</b>" (all lowercase) and the file is named "<b>index.html</b>".</p>
+    `);
+  }
+});
 
 let startTime = null;
 let isRunning = false;
@@ -38,11 +57,9 @@ io.on('connection', socket => {
     io.emit('timer_reset');
   });
 
-  // --- NEW LATENCY PING CHECK ---
   socket.on('ping_check', (timestamp, callback) => {
     callback(); 
   });
-  // ------------------------------
 
   socket.on('disconnect', () => {
     io.emit('connected_count', io.engine.clientsCount);
